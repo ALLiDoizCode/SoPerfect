99fdb5b0ac99c8428f4ef2f06645f2543146d057
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2016-03-31 10:06:11 -0400

    WIP

---
+++ src/Server/PerfectHandlers.swift
@@ -0,0 +1,24 @@
+import PerfectLib
+//public method that is being called by the server framework to initialise your module.
+public func PerfectServerModuleInit() {
+    
+    // Install the built-in routing handler.
+    // Using this system is optional and you could install your own system if desired.
+    Routing.Handler.registerGlobally()
+    
+    // Create Routes
+    Routing.Routes["GET", ["/", "index.html"] ] = { (_:WebResponse) in return IndexHandler() }
+    
+    // Check the console to see the logical structure of what was installed.
+    print("\(Routing.Routes.description)")
+}
+
+//Create a handler for index Route
+class IndexHandler: RequestHandler {
+    
+    func handleRequest(request: WebRequest, response: WebResponse) {
+        response.appendBodyString("Hello World")
+        response.requestCompletedCallback()
+    }
+}
+
---
+++ src/Server/SoPerfect.h
@@ -0,0 +1,19 @@
+//
+//  SoPerfect.h
+//  SoPerfect
+//
+//  Created by Jonathan Green on 3/29/16.
+//  Copyright © 2016 Jonathan Green. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SoPerfect.
+FOUNDATION_EXPORT double SoPerfectVersionNumber;
+
+//! Project version string for SoPerfect.
+FOUNDATION_EXPORT const unsigned char SoPerfectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoPerfect/PublicHeader.h>
+
+
